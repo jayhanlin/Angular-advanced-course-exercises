4a1862646cec453c555ea294acb9ae4915ae3d53
ed.lin <ed.lin@esunergy.com>
2021-04-24 13:58:13 +0800

    任務三完成 *講師作法

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { Article } from 'src/app/interfaces/article';
+import { PostService } from 'src/app/post.service';
 
 @Component({
   selector: 'app-post',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  article$: Observable<Article>;
+  article: Article;
+
+  constructor(private route: ActivatedRoute, private postService: PostService) { }
 
   ngOnInit(): void {
-  }
+    this.article$ = this.route.paramMap.pipe(
+      map(paramMap => paramMap.get('id')),
+      switchMap(id => this.postService.getArticle(id)),
+      map(singleArticle => singleArticle.article),
+      // shareReplay(1) // 在同一頁面打相同API時，可取得最近(1)次API內容
+    );
+      // 使用以上寫法時，因switchMap會subscribe，可避免以下寫法造成巢狀結構，不易閱讀、維護
 
+    // this.route.paramMap.subscribe(paramMap => {
+    //   const id = paramMap.get('id');
+    //   this.postService.getArticle(id).subscribe(singleArticle => {
+    //     this.article = singleArticle.article;
+    //     this.postService.getArticle(id).subscribe(singleArticle => {
+    //       this.postService.getArticle(id).subscribe(singleArticle => {
+    //       });
+    //     });
+    //   });
+    // })
+
+    // 延生閱讀以下差異
+    // switchMap, concatMap, mergeMag, exhaustMap
+
+  }
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Article } from '../interfaces/article';
+import { PostService } from './../post.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
+
+
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  articles: Article[];
+  articles$: Observable<Article[]>;
+
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.articles$ = this.postService.getArticles()
+      .pipe(
+        map(result => result.articles)
+      );
+    // this.postService.getArticles().subscribe(result => {
+    //   this.articles = result.articles;
+    // });
   }
 
 }
